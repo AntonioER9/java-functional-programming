a14d844856fbb0a88a6d04bd1023cd00935150c3
Antonio Espinoza <a0e0bhj@walmart.com>
2025-08-06 15:10:35 -0400

    Refactor Application class to implement Operation interface with an anonymous class for addition and add default method in Operation interface.

--- src/main/java/com/antonio/functional/programming/Application.java
+++ src/main/java/com/antonio/functional/programming/Application.java
@@ -8,6 +8,39 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+		// Operation operation = (a, b) -> a + b;
+		Operation operation = new Operation() {
+			/**
+			 * Es una clase anonima que implementa la interfaz Operation.
+			 * This method implements the operate method of the Operation interface.
+			 * It takes two integers and returns their sum.
+			 *
+			 * @param a the first integer
+			 * @param b the second integer
+			 * @return the sum of a and b
+			 */
+			@Override
+			public int operate(int a, int b) {
+				return a + b;
+			}
+		};
+
+		System.out.println("Result: " + operation.operate(5, 3));
 	}
 
+}
+
+@FunctionalInterface
+interface Operation {
+	int operate(int a, int b);
+
+	default String show() {
+		return "This is a default method in the Operation interface.";
+	}
+
+	//
+	static Operation substraction() {
+		return (a, b) -> a - b;
+	}
 }
